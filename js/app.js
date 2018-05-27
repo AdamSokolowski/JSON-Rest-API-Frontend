267c8897ac2abfee811a58c763ff6db6b2b2f070
AdamSokolowski <a.sokolowski84@gmail.com>
2018-05-27 15:15:57 +0200

    zadanie 3 warsztatu 4

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    var rootDiv = $("#root");
+
+    $.ajax({
+        url: "http://localhost:8282/books",
+        type: "GET"
+    }).done(function(data) {
+        console.log(data);
+
+        for(var i=0;i<data.length; i++){
+            var bookElement = $("<div>"+data[i].title+"</div>");
+            rootDiv.append(bookElement);
+        }
+    })
+
+});
