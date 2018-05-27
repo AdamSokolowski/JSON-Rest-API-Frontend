f9796b16aaf6fb41b84a4ead6a8494380a270a89
AdamSokolowski <a.sokolowski84@gmail.com>
2018-05-27 16:46:40 +0200

    Warsztat 4 zad5

--- js/app.js
+++ js/app.js
@@ -2,6 +2,58 @@ $(document).ready(function(){
 
     var rootDiv = $("#root");
 
+    refreshBooks(rootDiv);
+
+    handleForm();
+
+    rootDiv.on('click', '.book', function(){
+        console.log($(this).data("id"));
+        var bookDiv = $(this);
+        var detailDiv = bookDiv.find('div');
+        var bookId = $(this).data("id");
+
+        $.ajax({
+            url: "http://localhost:8282/books/" + bookId,
+            type: "GET"
+        }).done(function(bookDetails){
+            detailDiv.show();
+            detailDiv.text("Author: "+bookDetails.author + ", id"
+            + bookDetails.id+", isbn "+bookDetails.isbn + ", publisher "
+            +bookDetails.publisher +", type "+bookDetails.type);
+        })
+    })
+});
+
+function handleForm() {
+    var form = $('.new_book');
+    var submitButton = form.find("#add-button");
+    submitButton.on('click', function(event) {
+        event.preventDefault(); //zablokuj odświerzanie strony przez submita
+
+        var newBook ={};
+
+        newBook.author = $('#author').val();
+        newBook.isbn = $('#isbn').val();
+        newBook.publisher = $('#publisher').val();
+        newBook.title = $('#title').val();
+        newBook.type = $('#type').val();
+
+        $.ajax({
+            url:"http://localhost:8282/books",
+            type: "POST",
+            headers: {
+                'Accept': 'application/json',
+                "content-Type":"application/json"
+            },
+            data: JSON.stringify(newBook)
+        }).done(function(){
+            refreshBooks($('#root'));
+        })
+    })
+}  
+
+function refreshBooks(rootElement){
+    rootElement.html("");
     $.ajax({
         url: "http://localhost:8282/books",
         type: "GET"
@@ -9,9 +61,12 @@ $(document).ready(function(){
         console.log(data);
 
         for(var i=0;i<data.length; i++){
-            var bookElement = $("<div>"+data[i].title+"</div>");
-            rootDiv.append(bookElement);
+            var bookElement = $("<div class='book' data-id='"
+            +data[i].id+"'>"
+            +data[i].title+"<div style='display:none;background-color:grey;'></div>"
+            +"</div>");
+            rootElement.append(bookElement);
+
         }
     })
-
-});
+}
